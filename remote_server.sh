fa7fdde202c4b41114dee51e038b5076b337b19a
rizkyfaza20 <rizkyfaza61@gmail.com>
2020-07-06 14:34:07 +0700

    Changes in line 4

--- remote_server.sh
+++ remote_server.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Tools for Remote server
+
+function remote()
+{
 echo "Connect your server NoLimit via SSH Terminal"
 echo ""
 echo "-------------------------------------------"
@@ -10,27 +14,34 @@ select opt in Support Muara Care Dashboardv4 Analytic Exit; do
 
   case $opt in
 	Support)
+	  clear
 	  ssh support1
 	;;
 	Muara)
+	  clear
 	  ssh -t support1 "ssh -t muara"
 	;;
 	Care)
+	 clear
 	 ssh -t support1 "ssh -t care"
 	;;
 	Dashboardv4)
+	 clear
 	 ssh -t support1 "ssh -t root@beta.dashboard.nolimit.id"
 	;;
 	Analytic)
+    	 clear
 	 ssh -t support1 "ssh analytic"
 	;;
 	Exit)
 	exit 1
 	;;
 	*)
-	 echo "Sorry your input option not recognized. Exiting."
-	 exit 1
+	 echo "Sorry your input option not recognized."
 	;;
   esac
+REPLY=""
 done
+}
+remote
 
